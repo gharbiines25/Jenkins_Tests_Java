77fd1c321a9367114a5fc530a066f29ad400107a
gharbiines25 <gharbiines2021@gmail.com>
2025-06-11 10:33:57 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
 class Main {
   public static void main(String[] args) {
-     if(args[(args.length-1)].equals("+")){
-        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+    try {
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      String op = args[2];
+
+      switch (op) {
+        case "+":
+          System.out.println(a + b);
+          break;
+        case "-":
+          System.out.println(a - b);
+          break;
+        case "*":
+          System.out.println(a * b);
+          break;
+        case "/":
+          if (b == 0) {
+            System.out.println("Erreur : Division par zéro");
+            System.exit(-1);
+          } else {
+            System.out.println(a / b);
+          }
+          break;
+        default:
+          System.out.println("Opérateur invalide");
+          System.exit(-1);
       }
-      else if(args[(args.length-1)].equals("-")){
-        System.out.println(Integer.parseInt(args[0])-Integer.parseInt(args[1]));
-      }
-      else {System.exit(-1);}
+    } catch (NumberFormatException e) {
+      System.out.println("Erreur : Entrée non numérique");
+      System.exit(-1);
     }
+  }
 }
